f808876de0f70a03d36e14c25b5eefebe568e1dd
ethany04 <ethan.yu@utexas.edu>
2023-03-08 18:58:17 -0600

    added card flip **unfinished**

--- FrontEnd/Bazaart/SwipePage.js
+++ FrontEnd/Bazaart/SwipePage.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
-import { ImageBackground, Text, View } from 'react-native'
+import { ImageBackground, Text, View, SafeAreaView } from 'react-native'
 import TinderCard from 'react-tinder-card'
+import FlipCard from 'react-native-flip-card'
 
 const styles = {
-  container: {
+  safeView: {
+    backgroundColor: 'pink',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
+    flex: 1,
   },
   header: {
     color: '#000',
@@ -88,20 +91,31 @@ function Simple() {
   }
 
   return (
-    <SafeAreaView>
-        <View style={styles.container}>
+    <SafeAreaView style={styles.safeView}>
         <Text style={styles.header}>Find an Artist!</Text>
         <View style={styles.cardContainer}>
             {characters.map((character) =>
             <TinderCard key={character.name} onSwipe={(dir) => swiped(dir, character.name)} preventSwipe={['down']}>
-                <View style={styles.card}>
-                <ImageBackground style={styles.cardImage} source={character.img}>
-                    <Text style={styles.cardTitle}>{character.name}</Text>
-                </ImageBackground>
-                </View>
+                <FlipCard 
+                    friction={6}
+                    flipHorizontal={true}
+                    flipVertical={false}
+                    flip={false}
+                    clickable={true}
+                    onFlipEnd={(isFlipEnd)=>{console.log('isFlipEnd', isFlipEnd)}}
+                    >
+                    {/* Face Side */}
+                    <View style={styles.card}>
+                    <ImageBackground style={styles.cardImage} source={character.img}>
+                        <Text style={styles.cardTitle}>{character.name}</Text>
+                    </ImageBackground>
+                    </View>
+                    {/* Back Side */}
+                    <Text>Hello world</Text>
+                </FlipCard>
+                
             </TinderCard>
             )}
-        </View>
         </View>
     </SafeAreaView>
   )
