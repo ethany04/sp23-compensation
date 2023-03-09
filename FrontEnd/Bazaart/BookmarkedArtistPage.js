06beb0cdc209ae2e55dad66f0f72271e578c0fd5
Karner David <viEclipsekd@gmail.com>
2023-03-08 19:37:48 -0600

    BookmarkedArtist Page

---
+++ FrontEnd/Bazaart/BookmarkedArtistPage.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { ImageBackground, Text, View } from 'react-native'
+
+const styles = {
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+    },
+    artist_pfp: {
+
+    },
+    artist_name: {
+
+    },
+    artist_description: {
+
+    },
+    chat_icon: {
+
+    },
+    bookmark_icon: {
+
+    },
+    some_picture: {
+
+    },
+}
+
+return (
+    <View style={styles.container}>
+        <Text>
+            Bookmarked Artists
+            <View style={styles.artist_pfp}>
+               <Text>
+                    We pull artist pfp to here
+               </Text>
+            </View>
+            <View style={styles.artist_name}>
+                <Text>
+                    We pull artist name to here
+                </Text>
+            </View>
+            <View style={styles.artist_description}>
+                <Text>
+                    We pull artist description to here
+                </Text>
+            </View>
+            <View style={styles.chat_icon}>
+                <Text>
+                    <Image source=require />
+                    const img = new Image(100,200);
+                </Text>
+            </View>
+        </Text>
+    </View>
+    
+)
+
+
